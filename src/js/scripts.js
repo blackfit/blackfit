7c4ce3931d6018d5c6cc495353df997ebe3c4e8c
Igor Alisson <igoralissoncn@gmail.com>
2020-11-01 17:26:12 -0300

    versao 0.4 do app black

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,7 +10,7 @@ window.onload = function () {
     const maskCpf = IMask(cpf, maskOptions);
 
     busca.addEventListener("click", () => {
-        carregarTreino(maskCpf);
+        carregarTreino(maskCpf.masked.value);
     });
 };
 
@@ -19,10 +19,10 @@ function desativaCampo(idElement) {
     let obj = document.getElementById(idElement);
 
     if (!obj.style.color) {
-        obj.style.color = "#7A7A7A";
+        obj.classList.add('selected')
         upDownProgress(true);
     } else {
-        obj.style.color = null;
+        obj.classList.remove('selected')
         upDownProgress(false);
     }
 
@@ -30,21 +30,40 @@ function desativaCampo(idElement) {
 
 function upDownProgress(cond) {
 
+    const qtd_selected = document.querySelectorAll('.selected').length
+
     const statusBar = document.querySelector(".progress"); 
     statusBar.style.transition = "all 0.8s ease-in-out";
     
-    const per = document.querySelector(".treino").childElementCount;
+    const per = document.querySelector(".treino-aluno").childElementCount;
     const qtd = 172 / per;
 
     const valorAtual = (statusBar.style.padding).split('px')[0];
 
     const result = cond ? qtd + Math.round(valorAtual) : Math.round(valorAtual) - qtd;
+    const resultFinal = Math.round(Math.abs(result))
 
-    statusBar.style.padding = `${ Math.round(Math.abs(result)) }px`;  
+    statusBar.style.padding = `${ resultFinal }px`;  
+
+    const totalPerCent = Math.round(((100/per) * qtd_selected))
+
+    console.warn(`Total per cent: ${totalPerCent}`)
+    axios.patch('https://blackfitapp.herokuapp.com/api/alunos/progress', {
+        cpf: localStorage.getItem('cpf'),
+        dia: localStorage.getItem('dia'),
+        progresso: totalPerCent
+    })
+    .then(function (response) {
+        console.log(response.data)
+    })
+    .catch(function (error) {
+        console.log( { msg: error } )
+    })
 
     if (Math.round(result + 1) >= 172 ) {
         let name = document.getElementById("aluno").innerHTML;
         setTimeout(() => alert(`Parabéns ${name}, treino concluido!`), 800);
+
     }
 
 }
@@ -52,10 +71,10 @@ function upDownProgress(cond) {
 
 function carregarTreino(cpf) {
 
+    console.log(cpf)
+
     axios.get(`https://blackfitapp.herokuapp.com/api/alunos/${cpf}`)
     .then(function (response) {
-
-
         const dpns = document.querySelectorAll('.dpn')
 
         for(let dpn of dpns) {
@@ -64,11 +83,7 @@ function carregarTreino(cpf) {
 
         const card = document.querySelector(".card")
         card.remove()
- 
-        console.log(response.data)
-        
         montarTreino(response.data);
-
     })
     .catch(function (error) {
         console.log(error)
@@ -77,65 +92,75 @@ function carregarTreino(cpf) {
 }
 
 
-function salvarBrowser(cpf){
-    localStorage.setItem('id', cpf);
+function salvarBrowser(cpf, dia){
+    deleteBrowser()
+    localStorage.setItem('cpf', cpf)
+    localStorage.setItem('dia', dia)
+}
+
+function deleteBrowser(){
+    localStorage.removeItem('cpf')
+    localStorage.removeItem('dia')
 }
 
 function montarTreino(user) {
 
     const data = new Date(); 
 
-    const { nome, semana } = user
+    const { cpf, nome, semana } = user
     const { dia, serie, legenda } = semana[data.getDay()]
 
-    document.getElementById("dia").textContent = `${dia} -`;
-    document.getElementById("aluno").textContent = `${nome.toUpperCase()}`;
+   salvarBrowser(cpf, dia)
 
-    const raiz = document.querySelector(".treino");
+    document.getElementById("dia").textContent = `${dia} -`
+    document.getElementById("aluno").textContent = `${nome.toUpperCase()}`
+
+
+    const raiz = document.querySelector(".treino-aluno")
 
     for (let i in serie) {
 
-        let filhoTemp = document.createElement("li");
+        let filhoTemp = document.createElement("li")
 
-        let labelTemp = document.createElement("label");
-        labelTemp.setAttribute("class", "checkbox-label");
+        let labelTemp = document.createElement("label")
+        labelTemp.setAttribute("class", "checkbox-label")
 
-        let inputTemp = document.createElement("input");
-        inputTemp.setAttribute("type", "checkbox");
+        let inputTemp = document.createElement("input")
+        inputTemp.setAttribute("type", "checkbox")
 
-        let spanTemp = document.createElement("span");
-        spanTemp.setAttribute("class", "checkbox-custom");
+        let spanTemp = document.createElement("span")
+        spanTemp.setAttribute("class", "checkbox-custom")
 
-        let endId = `selecionar-${i}`;
-        filhoTemp.setAttribute("id", endId);
+        let endId = `selecionar-${i}`
+        filhoTemp.setAttribute("id", endId)
 
-        let spanText = document.createElement("span");
-        let text = document.createTextNode(serie[i]);
+        let spanText = document.createElement("span")
+        let text = document.createTextNode(serie[i])
 
-        spanText.appendChild(text);
-        filhoTemp.appendChild(spanText);
+        spanText.appendChild(text)
+        filhoTemp.appendChild(spanText)
 
-        filhoTemp.appendChild(labelTemp);
+        filhoTemp.appendChild(labelTemp)
 
-        inputTemp.setAttribute("onchange", `desativaCampo('${endId}')`);
+        inputTemp.setAttribute("onchange", `desativaCampo('${endId}')`)
 
-        labelTemp.appendChild(inputTemp);
-        labelTemp.appendChild(spanTemp);
+        labelTemp.appendChild(inputTemp)
+        labelTemp.appendChild(spanTemp)
 
-        raiz.appendChild(filhoTemp);
+        raiz.appendChild(filhoTemp)
     }
 
-    const ol = document.getElementById("legenda");
-    const legendas = legenda;
+    const ol = document.getElementById("legenda")
+    const legendas = legenda
 
 
     for (let t in legendas)  {
 
-        let elementLi = document.createElement("li");
-        let textLi = document.createTextNode(legendas[t]);
+        let elementLi = document.createElement("li")
+        let textLi = document.createTextNode(legendas[t])
 
-        elementLi.appendChild(textLi);
-        ol.appendChild(elementLi);
+        elementLi.appendChild(textLi)
+        ol.appendChild(elementLi)
     }
 
 }
